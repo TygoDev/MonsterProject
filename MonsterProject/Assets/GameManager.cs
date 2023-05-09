7a1b86728bddfe92d8359a24026035c3289adb73
TygoDev <tygohaan@gmail.com>
2023-05-09 12:10:57 +0200

    Game Manager + default scene
    Default scene is for everything we want to carry and keep between scene (like the game manager)

---
+++ MonsterProject/Assets/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
