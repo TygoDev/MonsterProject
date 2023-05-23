8f213904c1e8e78fac328d0054c50a971320a3de
Boldea Marius <yukki.github@gmail.com>
2023-05-23 14:24:32 +0200

    Rotating obstacles updated

--- MonsterProject/Assets/Scripts/RotatingObstacle.cs
+++ MonsterProject/Assets/Scripts/RotatingObstacle.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class RotatingObstacle : MonoBehaviour
 {
     public float speed;
+
+    public enum rotationAxis { 
+        X,
+        Y,
+        Z
+    }
+
+    public rotationAxis axis = rotationAxis.Z;
     void Update()
     {
-        this.transform.Rotate(0, 0, speed);
+        if(axis == rotationAxis.Z)
+            this.transform.Rotate(0, 0, speed);
+        
+        if(axis == rotationAxis.Y)
+            this.transform.Rotate(0, speed, 0);
+        
+        if(axis == rotationAxis.X)
+            this.transform.Rotate(speed, 0, 0);
     }
 }
