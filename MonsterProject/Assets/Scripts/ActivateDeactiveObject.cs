287669ee03e37838a0719f1815978fe8978c9e19
TygoDev <tygohaan@gmail.com>
2023-06-12 14:41:14 +0200

    Candy level update

--- MonsterProject/Assets/Scripts/ActivateDeactiveObject.cs
+++ MonsterProject/Assets/Scripts/ActivateDeactiveObject.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class ActivateDeactiveObject : MonoBehaviour
 {
     [SerializeField] private GameObject objectToChange;
+    [SerializeField] private List<GameObject> ObjectsToChange = new List<GameObject>();
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
-            objectToChange.SetActive(false);
+        if (other.CompareTag("Player"))
+        {
+            objectToChange.SetActive(boolToggle(objectToChange.activeInHierarchy));
+            if(ObjectsToChange.Count > 0)
+            {
+                foreach (GameObject obj in ObjectsToChange)
+                {
+                    obj.SetActive(boolToggle(obj.activeInHierarchy));
+                }
+            }
+
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
-            objectToChange.SetActive(true);
+        {
+            objectToChange.SetActive(objectToChange.activeInHierarchy);
+            if (ObjectsToChange.Count > 0)
+            {
+                foreach (GameObject obj in ObjectsToChange)
+                {
+                    obj.SetActive(boolToggle(boolToggle(obj.activeInHierarchy)));
+                }
+            }
+        }
+    }
+
+    bool boolToggle(bool boolean)
+    {
+        bool booleanVar = boolean;
+        return booleanVar = booleanVar ? false : true;
     }
 }
