093f1a430079826aa40b7d5eb4962789b1efcf31
Boldea Marius <yukki.github@gmail.com>
2023-06-13 14:00:55 +0200

    Particlesss

--- MonsterProject/Assets/Scripts/CandyScore.cs
+++ MonsterProject/Assets/Scripts/CandyScore.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CandyScore : MonoBehaviour
 {
     public int scoreValue;
 
+    [SerializeField] GameObject textPickUpPrefab;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag(Tags.T_Player))
+        if (other.CompareTag(Tags.T_Player))
         {
             GameManager.Instance.AddScore(scoreValue);
+            if (scoreValue > 0)
+                textPickUpPrefab.GetComponentInChildren<TMP_Text>().text = "+" + scoreValue;
+            else
+                textPickUpPrefab.GetComponentInChildren<TMP_Text>().text = "";
+            Instantiate(textPickUpPrefab, transform.position, Quaternion.identity);
         }
     }
 }
