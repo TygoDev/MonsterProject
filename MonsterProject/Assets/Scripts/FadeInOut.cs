a991e4ffb489d83300e05b93568f40ec757fbc24
Boldea Marius <yukki.github@gmail.com>
2023-06-13 12:10:32 +0200

    Objects fade

---
+++ MonsterProject/Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    [SerializeField] float fadeSpeed;
+    [SerializeField] float fadeAmount;
+
+    float initialOpacity;
+    Material mat;
+
+    public bool doFade;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().sharedMaterial;
+        initialOpacity = mat.color.a;
+    }
+
+    private void Update()
+    {
+        if (doFade)
+        {
+            FadeNow();
+        }
+        else
+        {
+            ResetFade();
+        }
+    }
+
+    private void ResetFade()
+    {
+        Color currentColor = mat.color;
+        Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, Mathf.Lerp(currentColor.a, fadeAmount, fadeSpeed));
+        mat.color = smoothColor;
+    }
+
+    private void FadeNow()
+    {
+        Color currentColor = mat.color;
+        Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, Mathf.Lerp(currentColor.a, initialOpacity, fadeSpeed));
+        mat.color = smoothColor;
+    }
+}
---
+++ MonsterProject/Assets/Scripts/RayForFade.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayForFade : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject[] players = new GameObject[2];
+
+    FadeInOut fade;
+    private void Update()
+    {
+        if(players != default)
+        {
+            foreach(var p in players)
+            {
+                Vector3 dir = p.transform.position - transform.position;
+                Ray ray = new Ray(transform.position, dir);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider == null)
+                        return;
+
+                    if(hit.collider.CompareTag(Tags.T_Player))
+                    {
+                        if(fade != null)
+                        {
+                            fade.doFade = false;
+                        }
+                    }
+                    else
+                    {
+                        fade = hit.collider.gameObject.GetComponent<FadeInOut>();
+                        if(fade != null)
+                        {
+                            fade.doFade = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
