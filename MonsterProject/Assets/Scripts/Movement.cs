4b8524637e6b2fdb8a7d31d96516ecb8550f3b0e
TygoDev <tygohaan@gmail.com>
2023-06-27 22:18:41 +0200

    stuff

--- MonsterProject/Assets/Scripts/Movement.cs
+++ MonsterProject/Assets/Scripts/Movement.cs
@@ -176,7 +176,7 @@ public class Movement : MonoBehaviour
         if(SceneManager.GetActiveScene().name == "Level_Candy")
         transform.position = new Vector3(transform.position.x, transform.position.y, 1f);
         else
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
     }
 
     private void ShowLostCanvas()
@@ -201,7 +201,7 @@ public class Movement : MonoBehaviour
         audioSource.Play();
         var footstep = Instantiate(footstepPrefab, transform.position - (Vector3)lastDirection / 2, Quaternion.identity);
         footstep.transform.rotation = Quaternion.Euler(0,0, Vector2.Angle(Vector2.up, lastDirection));
-        footstep.transform.position = new Vector3(footstep.transform.position.x, footstep.transform.position.y, 1.99f);
+        footstep.transform.position = new Vector3(footstep.transform.position.x, footstep.transform.position.y, 0.1f);
         yield return new WaitForSeconds(0.3f);
         coroutineForFootsteps = null;
     }
